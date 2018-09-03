cea782b9d918741ffa9ba3f0f8f6b132106a6d55
Duc Nguyen <anhduc12101992@gmail.com>
2018-09-03 22:29:36 +0900

    fixed major bugs causing wrong image unnormalization

--- sngan.py
+++ sngan.py
@@ -18,26 +18,26 @@ class DCGANGenerator(nn.Sequential):
         super(DCGANGenerator, self).__init__(input_shape=input_shape, layer_name=layer_name)
         self.append(nn.FullyConnectedLayer(self.output_shape, ch * bottom_width ** 2, layer_name=layer_name + '/fc1',
                                            activation='linear', init=nn.Normal(wscale)))
-        self.append(nn.BatchNormLayer(self.output_shape, layer_name+'/bn_fc1', activation=hidden_activation))
+        self.append(nn.BatchNormLayer(self.output_shape, layer_name+'/bn_fc1', activation=hidden_activation, epsilon=2e-5))
         self.append(nn.ReshapingLayer(self.output_shape, (-1, ch, bottom_width, bottom_width), layer_name+'/reshape'))
 
         shape = [o * 2 for o in self.output_shape[2:]]
         self.append(
             nn.TransposedConvolutionalLayer(self.output_shape, ch // 2, 4, shape, layer_name=layer_name + '/deconv1',
                                             padding=1, activation='linear', init=nn.Normal(wscale)))
-        self.append(nn.BatchNormLayer(self.output_shape, layer_name+'/bn_deconv1', activation=hidden_activation))
+        self.append(nn.BatchNormLayer(self.output_shape, layer_name+'/bn_deconv1', activation=hidden_activation, epsilon=2e-5))
 
         shape = [o * 2 for o in self.output_shape[2:]]
         self.append(
             nn.TransposedConvolutionalLayer(self.output_shape, ch // 4, 4, shape, layer_name=layer_name + '/deconv2',
                                             padding=1, activation='linear', init=nn.Normal(wscale)))
-        self.append(nn.BatchNormLayer(self.output_shape, layer_name + '/bn_deconv2', activation=hidden_activation))
+        self.append(nn.BatchNormLayer(self.output_shape, layer_name + '/bn_deconv2', activation=hidden_activation, epsilon=2e-5))
 
         shape = [o * 2 for o in self.output_shape[2:]]
         self.append(
             nn.TransposedConvolutionalLayer(self.output_shape, ch // 8, 4, shape, layer_name=layer_name + '/deconv3',
                                             padding=1, activation='linear', init=nn.Normal(wscale)))
-        self.append(nn.BatchNormLayer(self.output_shape, layer_name + '/bn_deconv3', activation=hidden_activation))
+        self.append(nn.BatchNormLayer(self.output_shape, layer_name + '/bn_deconv3', activation=hidden_activation, epsilon=2e-5))
 
         self.append(
             nn.TransposedConvolutionalLayer(self.output_shape, 3, 3, layer_name=layer_name + '/output', stride=(1, 1),
@@ -72,7 +72,7 @@ class SNDCGANDiscriminator(nn.Sequential):
                                   layer_name=layer_name + '/conv7', alpha=.2))
 
         self.append(nn.FullyConnectedLayer(self.output_shape, output_dim, nn.Normal(wscale), activation='linear',
-                                           layer_name=layer_name + '/output)'))
+                                           layer_name=layer_name + '/output'))
 
 
 def _l2normalize(v, eps=1e-12):
@@ -90,8 +90,8 @@ def max_singular_value(W, u=None, lp=1):
         u = theano.shared(np.random.normal(size=(1, W.get_value().shape[0])).astype('float32'), 'u')
     _u = u
     for _ in range(lp):
-        _v = _l2normalize(T.dot(_u, W), eps=1e-12)
-        _u = _l2normalize(T.dot(_v, W.T), eps=1e-12)
+        _v = _l2normalize(T.dot(_u, W))
+        _u = _l2normalize(T.dot(_v, W.T))
     sigma = T.sum(T.dot(T.dot(_u, W), _v.T))
     return sigma, _u, _v
 
@@ -105,7 +105,7 @@ def spectral_normalize(updates):
         else:
             u = theano.shared(np.random.normal(size=(1, key.get_value().shape[0])).astype('float32'), key.name + '/u')
             sigma, _u, _ = max_singular_value(param, u)
-            new_updates[key] = param / sigma
+            new_updates[key] = param / (sigma + 1e-12)
             new_updates[u] = _u
     return new_updates
 
@@ -118,26 +118,21 @@ class DataManager(nn.DataManager):
         self.n_epochs = int(self.batch_size * n_iters / self.data_size)
 
     def load_data(self):
-        X_train, _, _, _ = read_data.load_dataset(self.path)
-        self.dataset = np.float32(X_train / 255.)
+        X_train, _, X_test, _ = read_data.load_dataset(self.path)
+        X = np.concatenate((X_train, X_test))
+        self.dataset = np.float32(self.normalize(X / 255.))
         self.data_size = self.dataset.shape[0]
 
-    def normalize(self, input=None):
-        input = input if input is not None else self.dataset
+    def normalize(self, input):
         return (input - .5) * 2.
 
     def unnormalize(self, input=None):
-        input = input if input is not None else self.dataset
         return input / 2. + .5
-
-    @staticmethod
-    def preprocess(input):
-        return input.dimshuffle(0, 3, 1, 2) / 127.5 - 1.
 
 
 def train_sngan(z_dim=128, image_shape=(3, 32, 32), bs=64, n_iters=int(1e5), n_dis=5):
     gen = DCGANGenerator((None, z_dim))
-    dis = DCGANGenerator(gen.output_shape)
+    dis = SNDCGANDiscriminator(gen.output_shape)
 
     z = srng.uniform((bs, z_dim), -1, 1, ndim=2, dtype='float32')
     X = T.tensor4('image', 'float32')
@@ -162,6 +157,7 @@ def train_sngan(z_dim=128, image_shape=(3, 32, 32), bs=64, n_iters=int(1e5), n_d
     train_dis = nn.function([], dis_loss, updates=updates_dis_norm, givens={X: X_})
 
     # testing
+    nn.set_training_status(False)
     fixed_noise = T.constant(np.random.uniform(-1, 1, (bs, z_dim)), 'fixed noise', 2, 'float32')
     gen_imgs = gen(fixed_noise)
     generate = nn.function([], gen_imgs, name='generate images')
@@ -174,23 +170,23 @@ def train_sngan(z_dim=128, image_shape=(3, 32, 32), bs=64, n_iters=int(1e5), n_d
     start = time.time()
     for iteration in range(n_iters):
         #update generator
-        if iteration > 0:
-            training_gen_cost = train_gen()
-            if np.isnan(training_gen_cost) or np.isinf(training_gen_cost):
-                raise ValueError('Training failed due to NaN cost')
-            mon.plot('training gen cost', training_gen_cost)
+        training_gen_cost = train_gen()
+        if np.isnan(training_gen_cost) or np.isinf(training_gen_cost):
+            raise ValueError('Training failed due to NaN cost')
+        mon.plot('training gen cost', training_gen_cost)
 
         #update discriminator
+        training_disc_cost = []
         for i in range(n_dis):
             batches.__next__()
-            training_disc_cost = train_dis()
-            if np.isnan(training_disc_cost) or np.isinf(training_disc_cost):
+            training_disc_cost.append(train_dis())
+            if np.isnan(training_disc_cost[-1]) or np.isinf(training_disc_cost[-1]):
                 raise ValueError('Training failed due to NaN cost')
-            mon.plot('training disc cost', training_disc_cost)
+        mon.plot('training disc cost', np.mean(training_disc_cost))
 
         if iteration % 500 == 0:
             gen_images = generate()
-            mon.imwrite('generated image', gen_images, callback=dm.unnormalize)
+            mon.imwrite('generated image', dm.unnormalize(gen_images))
             mon.plot('time elapsed', (time.time() - start)/60.)
             mon.flush()
         mon.tick()
